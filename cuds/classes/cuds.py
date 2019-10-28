d8a33a07a237a08405fe0fb81ee75de56ba18e48
dea <pablo.de.andres@iwm.fraunhofer.de>
2019-10-28 17:25:51 +0100

    Flake8

--- cuds/classes/cuds.py
+++ cuds/classes/cuds.py
@@ -81,8 +81,8 @@ class Cuds(dict):
         :return: string with the official string representation for Cuds.
         """
         return "<%s: %s,  %s: @%s>" % (self.cuba_key, self.uid,
-                                      type(self.session).__name__,
-                                      hex(id(self.session)))
+                                       type(self.session).__name__,
+                                       hex(id(self.session)))
 
     def __getitem__(self, key):
         self.session._notify_read(self)
