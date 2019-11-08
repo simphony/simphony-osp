ac29ff1a8b5a8197a92dc2f03345c649ca9cb524
Matthias Urban <matthias.urban@iwm.fraunhofer.de>
2019-11-08 11:34:11 +0100

    fixed examples

--- examples/pretty_print.py
+++ examples/pretty_print.py
@@ -1,27 +1,25 @@
-from cuds.classes import (
-    City, Citizen, Neighbourhood, Street,
-    HasPart, HasInhabitant,
-    CUBA
-)
-from cuds.utils import pretty_print
+from osp.core import CITY
+from osp.core.utils import pretty_print
 
 
 # Let's build an EMMO compatible city!
-emmo_town = City('EMMO town', coordinates=[42, 42])
+emmo_town = CITY.CITY(name='EMMO town', coordinates=[42, 42])
 
-emmo_town.add(Citizen(name='Emanuele Ghedini'), rel=HasInhabitant)
-emmo_town.add(Citizen(name='Adham Hashibon'), rel=HasInhabitant)
-emmo_town.add(Citizen(name='Jesper Friis'), Citizen(name='Gerhard Goldbeck'),
-              Citizen(name='Georg Schmitz'), Citizen(name='Anne de Baas'),
-              rel=HasInhabitant)
+emmo_town.add(CITY.CITIZEN(name='Emanuele Ghedini'), rel=CITY.HAS_INHABITANT)
+emmo_town.add(CITY.CITIZEN(name='Adham Hashibon'), rel=CITY.HAS_INHABITANT)
+emmo_town.add(CITY.CITIZEN(name='Jesper Friis'),
+              CITY.CITIZEN(name='Gerhard Goldbeck'),
+              CITY.CITIZEN(name='Georg Schmitz'),
+              CITY.CITIZEN(name='Anne de Baas'),
+              rel=CITY.HAS_INHABITANT)
 
-emmo_town.add(Neighbourhood("Ontology"))
-emmo_town.add(Neighbourhood("User cases"))
+emmo_town.add(CITY.NEIGHBOURHOOD(name="Ontology"))
+emmo_town.add(CITY.NEIGHBOURHOOD(name="User cases"))
 
 ontology_uid = None
-for neighbourhood in emmo_town.get(cuba_key=CUBA.NEIGHBOURHOOD):
+for neighbourhood in emmo_town.get(oclass=CITY.NEIGHBOURHOOD):
     if neighbourhood.name == "Ontology":
         ontology_uid = neighbourhood.uid
-        neighbourhood.add(Street("Relationships"), rel=HasPart)
-        neighbourhood.add(Street("Entities"), rel=HasPart)
+        neighbourhood.add(CITY.STREET(name="Relationships"), rel=CITY.HAS_PART)
+        neighbourhood.add(CITY.STREET(name="Entities"), rel=CITY.HAS_PART)
 pretty_print(emmo_town)
