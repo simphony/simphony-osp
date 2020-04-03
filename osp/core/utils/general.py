b50bb71651ae5710e2013dde216f6f9ea487d742
Matthias Urban <matthias.urban@iwm.fraunhofer.de>
2020-04-03 16:52:02 +0200

    Added deserialize method in utils

--- osp/core/utils/general.py
+++ osp/core/utils/general.py
@@ -36,6 +36,18 @@ def get_rdf_graph(session=None):
 
 
 def post(url, cuds_object, max_depth=float("inf")):
+    """Will send the given CUDS object to the given URL. Will also send
+    the CUDS object in the container recursively.
+
+    Args:
+        url (string): The URL to send the CUDS object to
+        cuds_object (Cuds): The CUDS to send
+        max_depth (int, optional): The maximum depth to send CUDS objects
+            recursively. Defaults to float("inf").
+
+    Returns:
+        [type]: [description]
+    """
     from osp.core.utils import find_cuds_object
     from osp.core.session.transport.transport_util import serializable
     cuds_objects = find_cuds_object(criterion=lambda x: True,
@@ -47,6 +59,33 @@ def post(url, cuds_object, max_depth=float("inf")):
     return requests.post(url=url,
                          data=serialized,
                          headers={"content_type": "application/json"})
+
+
+def deserialize(json_doc, session=None):
+    """Deserialize the given json objects (to CUDS).
+    Will add the CUDS objects to the buffers.
+
+    Args:
+        json_doc (Union[str, dict]): the json document to load.
+            Either string or already loaded json object.
+        session (Session, optional): The session to add the CUDS objects to.
+            Defaults to the CoreSession.
+
+    Returns:
+        Any: The deserialized data. Can be CUDS.
+    """
+    from osp.core.cuds import Cuds
+    from osp.core.session.buffers import BufferContext
+    from osp.core.session.transport.transport_util import deserialize as x
+
+    if isinstance(json_doc, str):
+        json_doc = json.loads(json_doc)
+    session = session or Cuds._session
+    return x(
+        json_obj=json_doc,
+        session=session,  # The core session
+        buffer_context=BufferContext.USER
+    )
 
 
 def remove_cuds_object(cuds_object):
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,6 +8,7 @@
 import io
 import unittest
 import responses
+import uuid
 import osp.core
 from osp.core import CUBA
 from osp.core.session.transport.transport_util import serializable
@@ -19,7 +20,7 @@ from osp.core.utils import (
     create_recycle, create_from_cuds_object,
     check_arguments, format_class_name, find_cuds_object,
     find_cuds_object_by_uid, remove_cuds_object,
-    pretty_print,
+    pretty_print, deserialize,
     find_cuds_objects_by_oclass, find_relationships,
     find_cuds_objects_by_attribute, post,
     get_relationships_between,
@@ -32,6 +33,20 @@ try:
 except ImportError:
     from osp.core.ontology import Parser
     CITY = Parser().parse("city")
+
+CUDS_DICT = {
+    "oclass": "CITY.CITIZEN",
+    "uid": str(uuid.UUID(int=0)),
+    "attributes": {
+        "name": "Peter",
+        "age": 23
+    },
+    "relationships": {
+        "CITY.IS_INHABITANT_OF": {str(uuid.UUID(int=1)): "CITY.CITY"},
+        "CITY.HAS_CHILD": {str(uuid.UUID(int=2)): "CITY.PERSON",
+                           str(uuid.UUID(int=3)): "CITY.PERSON"}
+    }
+}
 
 
 def get_test_city():
@@ -84,6 +99,12 @@ class TestUtils(unittest.TestCase):
             i = serialized.index(x)
             del serialized[i]
         self.assertEqual(serialized, list())
+
+    def test_deserialize(self):
+        result = deserialize(CUDS_DICT)
+        self.assertTrue(result.is_a(CITY.CITIZEN))
+        self.assertEqual(result.name, "Peter")
+        self.assertEqual(result.age, 23)
 
     def test_destroy_cuds_object(self):
         """Test destroying of cuds"""
