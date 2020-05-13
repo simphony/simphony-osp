27224ce9bd078e0788501a177754756a8f8f429b
Matthias Urban <matthias.urban@iwm.fraunhofer.de>
2020-05-13 16:30:39 +0200

    Removed old copyright

--- osp/core/session/db/sql_wrapper_session.py
+++ osp/core/session/db/sql_wrapper_session.py
@@ -1,10 +1,3 @@
-# Copyright (c) 2014-2019, Adham Hashibon, Materials Informatics Team,
-# Fraunhofer IWM.
-# All rights reserved.
-# Redistribution and use are limited to the scope agreed with the end user.
-# No parts of this software may be used outside of this context.
-# No redistribution is allowed without explicit written permission.
-
 import uuid
 from operator import mul
 from functools import reduce
