1864f19a4bc7afb4fd177b28eb6adb2b26ee88b6
Matthias Urban <matthias.urban@iwm.fraunhofer.de>
2020-05-27 14:11:32 +0200

    remove java stuff for now

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 import os
-import subprocess
-import shutil
 from setuptools import setup, find_packages
 from packageinfo import VERSION, NAME
-from setuptools.command.develop import develop
-from setuptools.command.build_py import build_py
 
 
 # Read description
@@ -17,67 +13,6 @@ with open("packageinfo.py", "r") as packageinfo:
         for line in packageinfo:
             print(line, file=f, end="")
         print("# DO NOT MODIFY", file=f)
-
-
-def delete_dependencies():
-    so = os.path.join(os.path.dirname(__file__),
-                      "osp", "core", "java", "lib", "so")
-    jars = shutil.rmtree(os.path.join(os.path.dirname(__file__),
-                         "osp", "core", "java", "lib", "jars"))
-    shutil.rmtree(so)
-    shutil.rmtree(jars)
-    os.mkdir(so)
-    os.mkdir(jars)
-    open(os.path.join(so, "__init__.py")).close()
-    open(os.path.join(jars, "__init__.py")).close()
-
-
-def build_dependencies(force_dependency_download):
-    x = os.path.join(os.path.dirname(__file__), "download_dependencies.sh")
-    if force_dependency_download:
-        delete_dependencies()
-    subprocess.run(["bash", x], check=True)
-    try:
-        subprocess.run(["mvn", "-Dmaven.test.skip=true",
-                        "clean", "package", "-f", "osp/core/java/pom.xml"])
-        target_dir = os.path.join("osp", "core", "java", "target")
-        lib_dir = os.path.join("osp", "core", "java", "lib", "jars")
-        for file in os.listdir(target_dir):
-            if file.endswith(".jar"):
-                os.replace(os.path.join(target_dir, file),
-                           os.path.join(lib_dir, file))
-    except Exception as e:
-        raise RuntimeError(
-            "Error building parser. "
-            "Make sure Maven and JDK are installed.") from e
-
-
-class Develop(develop):
-    user_options = develop.user_options + [
-        ('force-dependency-download', None, 'Force the download of dependencies')
-    ]
-
-    def initialize_options(self):
-        develop.initialize_options(self)
-        self.force_dependency_download = ""
-
-    def run(self):
-        build_dependencies(self.force_dependency_download)
-        develop.run(self)
-
-
-class BuildPy(build_py):
-    user_options = build_py.user_options + [
-        ('force-dependency-download', None, 'Force the download of dependencies')
-    ]
-
-    def initialize_options(self):
-        build_py.initialize_options(self)
-        self.force_dependency_download = ""
-
-    def run(self):
-        build_dependencies(self.force_dependency_download)
-        build_py.run(self)
 
 
 # main setup configuration class
@@ -97,10 +32,6 @@ setup(
     },
     include_package_data=True,
     python_requires=">=3.6",
-    cmdclass={
-        'develop': Develop,
-        'build_py': BuildPy
-    },
     entry_points={
         'wrappers': 'osp-core = osp.core.session.core_session:CoreSession',
         'console_scripts': {
