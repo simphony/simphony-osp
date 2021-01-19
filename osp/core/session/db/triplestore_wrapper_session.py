0e75bdc25dd63ae21fe46f77f684c0c3c23111ab
Matthias Urban <matthias.urban@iwm.fraunhofer.de>
2021-01-19 14:55:52 +0100

    up

--- osp/core/session/db/triplestore_wrapper_session.py
+++ osp/core/session/db/triplestore_wrapper_session.py
@@ -5,7 +5,7 @@ import rdflib
 from osp.core.utils import create_from_triples
 from osp.core.utils import iri_from_uid, uid_from_iri, CUDS_IRI_PREFIX
 from osp.core.session.db.db_wrapper_session import DbWrapperSession
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 
 
 class TripleStoreWrapperSession(DbWrapperSession):
@@ -146,3 +146,56 @@ class TripleStoreWrapperSession(DbWrapperSession):
                     self._triples((o, rdflib.RDF.type, None))
                 )
         return triples, type_triples_of_neighbors
+
+    def sparql(self, query_string):
+        """Execute the given SPARQL query on the backend.
+
+        Args:
+            query_string (): The SPARQL query as a string.
+        """
+        return self._sparql(query_string=query_string.replace(
+            str(self.root), str(uuid.UUID(int=0))
+        ))
+
+    @abstractmethod
+    def _sparql(self, query_string):
+        pass
+
+
+class SparqlResult(ABC):
+    def __init__(self, session):
+        self.session = session
+
+    @abstractmethod
+    def close(self):
+        """Close the connection."""
+
+    @abstractmethod
+    def __iter__(self):
+        """Iterate the result."""
+
+    @abstractmethod
+    def __len__(self):
+        """Return the number of elements in the result."""
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+
+class SparqlBindingSet(ABC):
+    def __init__(self, session):
+        self.session = session
+
+    @abstractmethod
+    def _get(self, key):
+        pass
+
+    def __getitem__(self, key):
+        x = self._get(key)
+        if x is not None and x.startswith(CUDS_IRI_PREFIX) \
+                and uid_from_iri(x) == uuid.UUID(int=0):
+            return iri_from_uid(self.session.root)
+        return x
