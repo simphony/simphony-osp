13876ed9cdcee5c3ff915c013b8ae85f0e3b9b7e
José Manuel Domínguez <jose.manuel.dominguez@iwm.fraunhofer.de>
2021-03-09 13:27:09 +0100

    Changed the method to obtain the DOS path from `subprocess.run` to the ctypes library and a win32 API call. Hopefully fixes the security issue the CI is referring to.

--- osp/__init__.py
+++ osp/__init__.py
@@ -2,10 +2,11 @@ __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 # Patch RDFLib <= 5.0.0. See osp-core issue https://github.com/simphony/osp-core/issues/558 (the drive letter from the
 # path is stripped on Windows by the graph.Graph.serialize method of RDFLib <= 5.0.0).
-import rdflib
-import subprocess
-from urllib.parse import urlparse
 import os
+import ctypes
+import ctypes.wintypes as wintypes
+from urllib.parse import urlparse
+import rdflib
 
 
 def _compare_version_leq(version, other_version):
@@ -36,15 +37,25 @@ def _compare_version_leq(version, other_version):
 
 
 if _compare_version_leq(rdflib.__version__, '5.0.0'):
+    # Then patch RDFLib with the following decorator.
     def graph_serialize_fix_decorator(func):
         def graph_serialize(*args, **kwargs):
             if kwargs.get('destination') is not None and not hasattr(kwargs.get('destination'), 'write'):
+                # Bug causing case.
                 scheme, netloc, path, params, _query, fragment = urlparse(kwargs['destination'])
-                if urlparse(kwargs['destination']).path == r'\\':
-                    output = subprocess.run(['mountvol', f'{scheme}:\\', '/L'], stdout=subprocess.PIPE)
-                    dos_device_path = os.path.join(output.stdout.decode().strip().replace('?', '.'), path)
+                if urlparse(kwargs['destination']).path.startswith('\\'):  # The destination is a windows path.
+                    # Call the win32 API to get the volume ID.
+                    windows_func = ctypes.windll.kernel32.GetVolumeNameForVolumeMountPointW
+                    windows_func.argtypes = wintypes.LPCWSTR, wintypes.LPWSTR, wintypes.DWORD
+                    lpszVolumeMountPoint = wintypes.LPCWSTR(f'{scheme}:\\')
+                    lpszVolumeName = ctypes.create_unicode_buffer(50)
+                    cchBufferLength = wintypes.DWORD(50)
+                    windows_func(lpszVolumeMountPoint, lpszVolumeName, cchBufferLength)
+                    # Get a DOS_DEVICE_PATH, not affected by the drive letter stripping bug.
+                    dos_device_path = os.path.join(lpszVolumeName.value.replace('?', '.')
+                                                   or f'\\\\.\\Volume{{DRIVE_LETTER_{scheme}_NOT_ASSIGNED}}\\',
+                                                   path)
                     kwargs['destination'] = dos_device_path
             func(*args, **kwargs)
         return graph_serialize
     rdflib.Graph.serialize = graph_serialize_fix_decorator(rdflib.Graph.serialize)
-
