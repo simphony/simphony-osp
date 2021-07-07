ea9ce17d588ce57ce017ae33689b8dd207b44172
MBueschelberger <46421269+MBueschelberger@users.noreply.github.com>
2021-07-07 17:25:19 +0200

    Fetch datatypes from Sparql-Result (#663)
    Closes #655. Makes SparqlResult callable, as in `result(specimen='cuds', cycles=float)`, so that for example `next(result(specimen='cuds', cycles=float))['specimen']` returns a CUDS object instead of an rdflib URIRef.
    
    Co-authored-by: José Manuel Domínguez <jose.manuel.dominguez@iwm.fraunhofer.de>

--- osp/core/session/sparql_backend.py
+++ osp/core/session/sparql_backend.py
@@ -69,6 +69,15 @@ class SparqlResult(ABC):
         """Enter the with statement."""
         return self
 
+    def __call__(self, **kwargs):
+        """Add kwargs to datatypes when class is called."""
+        # Set the datatypes of each returned SparqlBindingSet while keeping
+        # __iter__ as an abstract method.
+        def add_datatypes(x):
+            x.datatypes = kwargs
+            return x
+        return map(add_datatypes, self.__iter__())
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         """Close the connection."""
         self.close()
@@ -77,9 +86,10 @@ class SparqlResult(ABC):
 class SparqlBindingSet(ABC):
     """A base class from wrapper rows in SPARQL results."""
 
-    def __init__(self, session):
+    def __init__(self, session, datatypes=None):
         """Initialize the object."""
         self.session = session
+        self.datatypes = datatypes or dict()
 
     @abstractmethod
     def _get(self, variable_name):
@@ -89,10 +99,57 @@ class SparqlBindingSet(ABC):
     def __getitem__(self, variable_name):
         """Get the value of the given variable.
 
-        Handle wrapper IRIs.
+        Handles wrapper IRIs and datatype conversion.
         """
-        x = self._get(variable_name)
-        if x is not None and x.startswith(CUDS_IRI_PREFIX) \
-                and uid_from_iri(x) == uuid.UUID(int=0):
-            return iri_from_uid(self.session.root)
-        return x
+        iri = self._get(variable_name)
+        if iri is not None and iri.startswith(CUDS_IRI_PREFIX) \
+                and uid_from_iri(iri) == uuid.UUID(int=0):
+            iri = iri_from_uid(self.session.root)
+        return self._check_datatype(variable_name, iri)
+
+    def _check_datatype(self, variable_name, iri):
+        """Check if iri shall be converted to a certain datatype.
+
+        The `variable_name` is checked against the dictionary `self.datatypes`,
+        and if a datatype is defined there for such variable name, then the
+        function returns the value of the variable converted to such datatype.
+
+        Args:
+            variable_name (str): the variable of the SPARQL query on which the
+                check should be performed.
+            iri (Union[URIRef, Literal]): a result returned by the SPARQL
+                query for such variable name. This is what is then converted
+                to the desired datatype.
+
+        Returns:
+            Any: the result of the SPARQL query converted to the desired
+                datatype.
+
+        Raises:
+            TypeError: when the an invalid string is specified as target
+                datatype or the target datatype is neither a string or a
+                callable.
+
+            ValueError: when there is an exception on the conversion process.
+        """
+        if iri is None or not self.datatypes:
+            return iri
+        variable_type = self.datatypes.get(variable_name)
+        if variable_type is None:
+            return iri
+
+        unknown_type_error = TypeError(f"Variable type {variable_type} not "
+                                       f"understood.")
+        try:
+            if variable_type == 'cuds':
+                cuds_query = self.session.load_from_iri(iri)
+                return cuds_query.first()
+            elif callable(variable_type):
+                return variable_type(iri)
+            else:
+                raise unknown_type_error
+        except Exception as exception:
+            if exception is not unknown_type_error:
+                raise ValueError(exception) from exception
+            else:
+                raise unknown_type_error
