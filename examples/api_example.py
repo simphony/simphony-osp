c57d92ecf97dd2ac5b3103054a06876c68f81155
José Manuel Domínguez <jose.manuel.dominguez@iwm.fraunhofer.de>
2021-10-01 16:50:43 +0200

    Update `api_example.py`.

--- examples/api_example.py
+++ examples/api_example.py
@@ -2,69 +2,112 @@
 
 # Please install the city ontology: $pico install city
 
-# If you did not install the city ontology
-# (pico install city),
-# you have to execute these commands first:
-# from osp.core import Parser
-# p = Parser()
-# p.parse("city")
-
 from osp.core.namespaces import city
 
 print("Creating a City object, c...")
 c = city.City(name="Freiburg", coordinates=[47, 7])
-print("  uid of c: " + str(c.uid))
-print("  IRI of c: " + str(c.iri))
-print("  oclass of c: " + str(c.oclass) + "\n")
-
 print("Creating Citizen objects, p1, p2...")
 p1 = city.Citizen(name="Peter")
 p2 = city.Citizen(name="Anne")
+
+# Functionalities exposed as Python properties.
+
+print("Retrieving uid, IRIs and ontology classes...")
+
+print("  uid of c: " + str(c.uid))
 print("  uid of p1: " + str(p1.uid))
-print("  IRI of p1: " + str(p1.iri))
-print("  oclass of p1: " + str(p1.oclass) + "\n")
 print("  uid of p2: " + str(p2.uid))
+
+print("  IRI of c: " + str(c.iri))
+print("  IRI of p1: " + str(p1.iri))
 print("  IRI of p2: " + str(p2.iri))
-print("  oclass of p2: " + str(p2.oclass) + "\n")
+
+print("  oclass of c: " + str(c.oclass))
+print("  oclass of p1: " + str(p1.oclass))
+print("  oclass of p2: " + str(p2.oclass))
 
 print("Checking attributes of the CUDS objects...")
-print(f"Name of c: {c.name}. Coordinates of c: {c.coordinates}.")
-print("Name of p1: " + str(p1.name))
-print("Name of p2: " + str(p2.name))
+print(f"  Name of c: {c.name}. Coordinates of c: {c.coordinates}.")
+print("  Name of p1: " + str(p1.name))
+print("  Name of p2: " + str(p2.name))
 
 print("\nChanging the attribute values of the CUDS objects...")
-print(f"Change the name of {p1.name}.")
+print(f"  Change the name of {p1.name}.")
 p1.name = "Bob"
-print(f"Name of p1: {p1.name}.")
+print(f"  Name of p1: {p1.name}.")
+
+# Functionalities exposed as Python methods.
 
 print("\nAdding p1 to c...")
 c.add(p1, rel=city.hasInhabitant)
-print("internal dict of c:", c._neighbors, "\n")
 
 print("Adding p2 to c...")
 c.add(p2, rel=city.hasInhabitant)
-print("internal dict of c:", c._neighbors, "\n")
 
 print("\nElements in c:")
 for el in c.iter():
     print("  uid: " + str(el.uid))
 
 print("\nGetting p1 from c:")
-print(c.get(p1.uid))
+print(f"  {c.get(p1.uid)}")
 
 print("\nGetting city.Citizen from c:")
-print(c.get(oclass=city.Citizen))
+print(f"  {c.get(oclass=city.Citizen)}")
 
 print("\n Remove p1:")
 c.remove(p1.uid)
-print("internal dict of c:", c._neighbors, "\n")
 
 print("\nAdding neighborhoods to Cuds object in a loop:")
 for i in range(6):
-    print("Added neighborhood %s" % i)
+    print("  Added neighborhood %s" % i)
     c.add(city.Neighborhood(
         name="neighborhood %s" % i))
-print("internal dict of c:", c._neighbors, "\n")
 
-print('Trying out the `is_a` method trivially with the new neighborhoods.')
-print(all(n.is_a(city.Neighborhood) for n in c.get(oclass=city.Neighborhood)))
+print('\nTrying out the `is_a` method trivially with the new neighborhoods.')
+print("  %s" %
+      all(n.is_a(city.Neighborhood) for n in c.get(oclass=city.Neighborhood)))
+
+# Functionalities exposed through subscripting.
+
+print("\nAdd, get and remove a single cuds (non-deterministic) using "
+      "subscripting.")
+print(f"  {c[city.hasInhabitant].name}")
+c[city.hasInhabitant] = p1
+print(f"  {c[city.hasInhabitant].name}")
+c[city.hasInhabitant] = None
+print(f"  {c[city.hasInhabitant]}")
+c[city.hasInhabitant] = p1
+del c[city.hasInhabitant]
+print(f"  {c[city.hasInhabitant]}")
+
+print("\nAdd, get and remove multiple cuds using subscripting. The result of "
+      "using a slice as second argument behaves like a Python set, but has "
+      "some additional capability to perform in-place operations on single "
+      "objects as well.")
+print(f"  p1: {p1}")
+print(f"  p2: {p2}")
+c[city.hasInhabitant, :] = {p1, p2}
+print(f"  {c[city.hasInhabitant, :]}")
+c[city.hasInhabitant, :] -= p1
+print(f"  {c[city.hasInhabitant, :]}")
+c[city.hasInhabitant, :] += p1
+print(f"  {c[city.hasInhabitant, :]}")
+c[city.hasInhabitant, :] = {p2}
+print(f"  {c[city.hasInhabitant, :]}")
+c[city.hasInhabitant, :] ^= {p2}
+print(f"  {c[city.hasInhabitant, :]}")
+print(f"  {c[city.hasInhabitant, :] | {p1, p2}}")
+print(f"  {c[city.hasInhabitant, :]}")
+
+print("\nThe subscripting also works for attributes. In particular, it can be "
+      "used to assign multiple values to the same attribute.")
+c[city.name, :] = {'Freiburg', 'Freiburg im Breisgau'}
+print(f"  {c[city.name, :]}")
+print("  Be aware that when multiple values are assigned, the dot notation "
+      "will raise an exception.")
+c[city.name, :] += {'Stadt', 'City'}
+print(f"  {c[city.name, :]}")
+print(f"  {c[city.name]}")
+# c.name -> Exception
+c[city.name, :] -= {'Stadt', 'City', 'Freiburg'}
+print(f"  {c[city.name, :]}")
