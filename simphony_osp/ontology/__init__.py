d07d7da5578145b56cf70931b1cfce447d800c19
José Manuel Domínguez <jose.manuel.dominguez@iwm.fraunhofer.de>
2022-09-09 18:01:24 +0200

    Add members to the `simphony_osp.ontology` module.

--- simphony_osp/ontology/__init__.py
+++ simphony_osp/ontology/__init__.py
@@ -2,17 +2,37 @@
 
 from simphony_osp.ontology.annotation import OntologyAnnotation
 from simphony_osp.ontology.attribute import OntologyAttribute
+from simphony_osp.ontology.composition import OPERATOR as COMPOSITION_OPERATOR
 from simphony_osp.ontology.composition import Composition
 from simphony_osp.ontology.entity import OntologyEntity
-from simphony_osp.ontology.individual import OntologyIndividual
+from simphony_osp.ontology.individual import (
+    AnnotationSet,
+    AttributeSet,
+    OntologyIndividual,
+    RelationshipSet,
+    MultipleResultsError,
+    ResultEmptyError,
+)
 from simphony_osp.ontology.namespace import OntologyNamespace
 from simphony_osp.ontology.oclass import OntologyClass
 from simphony_osp.ontology.relationship import OntologyRelationship
+from simphony_osp.ontology.restriction import (
+    QUANTIFIER as RESTRICTION_QUANTIFIER,
+)
+from simphony_osp.ontology.restriction import RTYPE as RESTRICTION_TYPE
 from simphony_osp.ontology.restriction import Restriction
 
 __all__ = [
+    "AnnotationSet",
+    "AttributeSet",
     "Composition",
+    "COMPOSITION_OPERATOR",
+    "MultipleResultsError",
+    "RESTRICTION_QUANTIFIER",
+    "RESTRICTION_TYPE",
+    "RelationshipSet",
     "Restriction",
+    "ResultEmptyError",
     "OntologyAnnotation",
     "OntologyAttribute",
     "OntologyEntity",
