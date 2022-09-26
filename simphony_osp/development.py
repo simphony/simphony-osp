180de4cb6b2f19e366973ccb1c388a9bc48b72af
José Manuel Domínguez <43052541+kysrpex@users.noreply.github.com>
2022-09-26 15:20:24 +0200

    Improve documentation of Wrapper API methods (#823)
    * Improve documentation of Wrapper API methods.
    
    * Include `BufferType` in `simphony_osp.development` module.

--- simphony_osp/development.py
+++ simphony_osp/development.py
@@ -1,7 +1,14 @@
 """Developer tools."""
 
+from simphony_osp.interfaces.interface import BufferType
 from simphony_osp.interfaces.interface import Interface as Wrapper
 from simphony_osp.interfaces.remote.common import get_hash
 from simphony_osp.ontology.operations import Operations, find_operations
 
-__all__ = ["Operations", "Wrapper", "find_operations", "get_hash"]
+__all__ = [
+    "BufferType",
+    "Operations",
+    "Wrapper",
+    "find_operations",
+    "get_hash",
+]
